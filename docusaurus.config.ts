f44e9c4be3d5f6a5064508fe282110485146b1d4
yashodgayashan <yashodgayashan@gmail.com>
2026-08-30 10:16:53 +0530

    docs: update 1.2.4 version in version docs
    Signed-off-by: yashodgayashan <yashodgayashan@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -229,9 +229,9 @@ const config: Config = {
 
   themeConfig: {
     announcementBar: {
-      id: 'release_v1_2_3',
+      id: 'release_v1_2_4',
       content:
-        '🎉️ OpenChoreo <a target="_blank" rel="noopener noreferrer" href="https://github.com/openchoreo/openchoreo/releases/tag/v1.2.3">v1.2.3</a> has been released! 🎉',
+        '🎉️ OpenChoreo <a target="_blank" rel="noopener noreferrer" href="https://github.com/openchoreo/openchoreo/releases/tag/v1.2.4">v1.2.4</a> has been released! 🎉',
       isCloseable: true,
     },
     docsearch: {
